4e72909a7decef91c7af7489fb1bddc7560c754f
Vinicios Neves <viniciosneves16@gmail.com>
2023-11-18 18:19:49 -0300

    utilizando join e where

--- aprendendo_parte1.sql
+++ aprendendo_parte1.sql
@@ -1,6 +1,3 @@
-INSERT INTO comentarios(id_comentario, id_usuario, titulo, comentario)
-VALUES (1, 2, 'minha primeira vez neste site', 'Olá está é a minha primeira conta neste site, e gosto muito desta comunidade, pois todos se ajudam e tiram duvidas entre si, aonde o mais experiente ajuda a solucionar as duvidas, as vezes tem um pouco de comflito quando duas pessoas experientes tentam ajudar, porem a forma de ajuda e completamente distinta, mas o resultado ainda é a mesma');
-
-SELECT comentarios.id_usuario, comentarios.titulo, comentarios.comentario, usuarios.id_usuario
-FROM comentarios
-JOIN usuarios ON comentarios.id_usuario = usuarios.id_usuario;
+SELECT usuarios.nome_usuario, comentarios.titulo, comentarios.comentario
+FROM comentarios, usuarios
+WHERE comentarios.id_usuario = 1 AND usuarios.id_usuario = 1;
