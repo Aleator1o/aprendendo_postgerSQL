ed87a7e78369ab25ac597b15bc6371de33724909
Vinicios Neves <viniciosneves16@gmail.com>
2023-11-18 17:57:43 -0300

    criando tabela comentario e add um comentario

--- aprendendo_parte1.sql
+++ aprendendo_parte1.sql
@@ -1,4 +1,6 @@
-INSERT INTO usuarios(id_usuario, nome_usuario, email, senha)
-VALUES (2, 'Armelia', 'Armeliapereira@gmail.com', 59784532);
+INSERT INTO comentarios(id_comentario, id_usuario, titulo, comentario)
+VALUES (1, 2, 'minha primeira vez neste site', 'Olá está é a minha primeira conta neste site, e gosto muito desta comunidade, pois todos se ajudam e tiram duvidas entre si, aonde o mais experiente ajuda a solucionar as duvidas, as vezes tem um pouco de comflito quando duas pessoas experientes tentam ajudar, porem a forma de ajuda e completamente distinta, mas o resultado ainda é a mesma');
 
-SELECT * FROM usuarios;
+SELECT comentarios.id_usuario, comentarios.titulo, comentarios.comentario, usuarios.id_usuario
+FROM comentarios
+JOIN usuarios ON comentarios.id_usuario = usuarios.id_usuario;
